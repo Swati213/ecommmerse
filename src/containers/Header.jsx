e0ec9afda926339a403d23636708a790ac4340e1
unknown <s4520sinha@gmail.com>
2021-12-16 13:44:02 +0530

    ecommerse

---
+++ src/containers/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='ui fixed menu'>
+            <div className="ui container center">
+<h2>
+    Holding Online Shop 👍
+</h2>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
